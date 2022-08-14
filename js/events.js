b5667bd710f92c6df79544b598e7d2d93e10635c
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-14 09:40:47 +0600

    Add onclick handler

--- js/events.js
+++ js/events.js
@@ -1,1 +1,5 @@
 console.log('Hello Event');
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
